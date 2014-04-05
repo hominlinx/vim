aaed5f21929fbb5f2cb3422e6b857247592b0af6
yuanguang <yuanguang@pset.suntec.net>
2014-04-05 15:30:55 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -5,19 +5,19 @@ cd $BASEDIR
 CURRENT_DIR=`pwd`
 
 lnif() {
-    if [-e "$1"]; then
+    if [ -e "$1" ]; then
         ln -sf "$1" "$2"
     fi
 }
 
 echo "1-->backing up current vim config"
 today=`date +%Y%m%d`
-for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [-e $i] && [! -L $i] && mv $i $i.$today; done
-for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [! -L $i] && unlink $i ; done
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today; done
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ ! -L $i ] && unlink $i ; done
 
 echo "2-->setting up symlinks"
 lnif $CURRENT_DIR/vimrc $HOME/.vimrc
-lnif "$CURRENT_DIR/vim" "$HOME/.vim"
+lnif $CURRENT_DIR/../vim $HOME/.vim
 
 echo "3-->compile YCM"
 echo "It will take some time, please be patient^^^^^^^^^^"
